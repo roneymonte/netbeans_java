437889e5fa7c8e436d9101067674b52de521e7a9
roney <roney@i3dell>
2015-07-27 17:04:29 -0300

    Projeto Inicial JAVA com Netbeans

---
+++ proj01_JavaApplication1/src/proj01_javaapplication1/Proj01_JavaApplication1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proj01_javaapplication1;
+
+/**
+ *
+ * @author roney monte
+ * 
+ */
+public class Proj01_JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
