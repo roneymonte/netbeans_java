411a270b17c9ee6141230cfb3aaa9e55c36f97da
roney <roney@i3dell>
2015-07-27 18:15:59 -0300

    Classes Involucro e conversoes de tipos com Java e Netbeans

---
+++ proj05_JavaApplication4_testeTipos/src/proj05_javaapplication4_testetipos/Proj05_JavaApplication4_testeTipos.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proj05_javaapplication4_testetipos;
+
+/**
+ *
+ * @author roney
+ */
+public class Proj05_JavaApplication4_testeTipos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int idade = 30;
+        String valor = Integer.toString(idade);
+        System.out.println(valor);
+        
+        String numero = "44";
+        int tempo = Integer.parseInt (numero);
+        System.out.println(tempo);
+    }
+    
+}
